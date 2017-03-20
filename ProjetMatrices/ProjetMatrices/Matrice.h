e311de2bfb0b0652abff177e2840baa38d687058
GouVsky <greg6-marco@hotmail.fr>
2017-03-20 22:31:38 +0100

    Documentation de CMatrice.hpp.

--- ProjetMatrices/ProjetMatrices/Matrice.h
+++ ProjetMatrices/ProjetMatrices/Matrice.h
@@ -203,7 +203,7 @@ public:
 	Opérateur / à paramètre de type T.
 	******************************************
 	Entree : la valeur à diviser (de type T).
-	Necessite : rien.
+	Necessite : tValeur doit être différente de 0.
 	Sortie : instance de la classe CMatrice contenant le résultat de la division.
 	Entraîne : Allocation d'un nouvel objet CMatrice,
 			   division de la valeur passée en paramètre avec toutes les cases de la matrice.
@@ -240,153 +240,213 @@ public:
 
 	/*****************************************
 	Lecture du nombre de lignes.
-	*****************************************
+	******************************************
 	Entrée : rien.
 	Nécessite : rien.
 	Sortie : le nombre de lignes de la matrice (unsigned int).
 	Entraîne : rien.
-	*****************************************
-	*/
+	******************************************/
+
 	unsigned int MATgetNbLignes() const;
 
-	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
-	T & MATgetValeur(unsigned int uiLigne, unsigned int uiColonne);
 
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Lecture de la valeur d'un coefficient.
+	******************************************
+	Entéee : l'indice de la ligne (unsigned int),
+			 l'indice de la colonne (unsigned int).
+	Nécessite : rien.
+	Sortie : le coefficient à la position (uiLigne, uiColonne).
+	Entraîne : rien.
+	******************************************/
+
+	T & MATgetValeur(unsigned int uiLigne, unsigned int uiColonne);
+
+
+	/*****************************************
+	Lecture d'une ligne.
+	******************************************
+	Entrée : l'indice de la ligne (unsigned int).
+	Nécessite : rien.
+	Sortie : la ligne à l'indice uiLigne.
+	Entraîne : rien.
+	******************************************/
+
 	T * MATgetLigne(unsigned int uiLigne);
 
 	
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Lecture d'une colonne.
+	******************************************
+	Entrée : l'indice de la colonne (unsigned int).
+	Nécessite : rien.
+	Sortie : la colonne à l'indice uiColonne.
+	Entraîne : rien.
+	******************************************/
+
 	T * MATgetColonne(unsigned int uiColonne);
 
 
 	// ----- Setters ----------------------------------------
 
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Mutateur pour la valeur.
+	******************************************
+	Entrée : l'indice de la ligne (unsigned int),
+			 l'indice de la colonne (unsigned int),
+			 la valeur définir (de type T).
+	Nécessite : rien.
+	Sortie : rien.
+	Entraîne : un changement de valeur du coefficient (uiLigne, uiColonne).
+	******************************************/
+
 	void MATsetValeur(unsigned int uiLigne, unsigned int uiColonne, T tValeur);
 
-	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
-	void MATsetNbLignes(unsigned int uiNbLignes);
 
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Mutateur pour le nombre de lignes.
+	******************************************
+	Entrée : le nombre de lignes (unsigned int).
+	Nécessite : rien.
+	Sortie : rien.
+	Entraîne : une initialisation ou modification du nombre de lignes de la matrice.
+	******************************************/
+
+	void MATsetNbLignes(unsigned int uiNbLignes);
+
+
+	/*****************************************
+	Mutateur pour le nombre de colonnes.
+	******************************************
+	Entrée : le nombre de colonnes (unsigned int).
+	Nécessite : rien.
+	Sortie : rien.
+	Entraîne : une initialisation ou modification du nombre de colonnes de la matrice.
+	******************************************/
+
 	void MATsetNbColonnes(unsigned int uiNbColonnes);
+
 
 	// ----- Calculs ----------------------------------------
 
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Le rang de la matrice.
+	******************************************
+	Entrée : rien.
+	Nécessite : rien.
+	Sortie : le rang de la matrice (unsigned int).
+	Entraîne : rien.
+	******************************************/
+
 	unsigned int MATrang();
 
+
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Echelonnement de la matrice.
+	******************************************
+	Entrée : rien.
+	Nécessite : rien.
+	Sortie : une instance de CMatrice.
+	Entraîne : l'échelonnement de la matrice.
+	******************************************/
+
 	CMatrice<T> MATechelonnee();
 
+
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Transposée de la matrice.
+	******************************************
+	Entrée : rien.
+	Nécessite : rien.
+	Sortie : une instance de CMatrice.
+	Entraîne : une transposition de la matrice.
+	******************************************/
+
 	CMatrice<T> MATtransposee();
 
-	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
-	CMatrice<T> MATsousMatrice();
 
 	/*****************************************
-	Constructeur par defaut
-	*****************************************
-	Entree : void
-	Necessite : rien
-	Sortie : rien
-	Entraine : Initialisation de l'objet
-	*****************************************
-	*/
+	Calcul d'une sous-matrice.
+	******************************************
+	Entrée : rien.
+	Nécessite : rien.
+	Sortie : une instance de CMatrice.
+	Entraîne : //
+	******************************************/
+
+	CMatrice<T> MATsousMatrice();
+
+
+	/*****************************************
+	Test de matrice nulle.
+	******************************************
+	Entrée : rien.
+	Nécessite : rien.
+	Sortie : un booléen.
+	Entraine : (true : la matrice est nulle)
+			   OU (false : la matrice n'est pas nulle).
+	******************************************/
+
 	bool MATestNulle();
 };
 
+
 // Opérateurs complémentaires
+
+/*****************************************
+Opérateur +.
+******************************************
+Entrée : la valeur à ajouter (de type T),
+		 une instance de CMatrice.
+Nécessite : rien.
+Sortie : instance de la classe CMatrice contenant le résultat de la somme.
+Entraîne : Allocation d'un nouvel objet CMatrice,
+		   ajout de la valeur passée en paramètre à toutes les cases de la matrice.
+******************************************/
 
 template <class T>
 CMatrice<T> operator+(const T tValeur, const CMatrice<T> & MATmatrice);
 
+
+/*****************************************
+Opérateur -.
+******************************************
+Entrée : la valeur à soustraire (de type T),
+		 une instance de CMatrice.
+Nécessite : rien.
+Sortie : instance de la classe CMatrice contenant le résultat de la soustraction.
+Entraîne : Allocation d'un nouvel objet CMatrice,
+		   soustraction de la valeur passée en paramètre avec toutes les cases de la matrice.
+******************************************/
+
 template <class T>
 CMatrice<T> operator-(const T tValeur, const CMatrice<T> & MATmatrice);
 
+
+/*****************************************
+Opérateur *.
+******************************************
+Entrée : la valeur à multiplier (de type T),
+		 une instance de CMatrice.
+Nécessite : rien.
+Sortie : instance de la classe CMatrice contenant le résultat du produit.
+Entraîne : Allocation d'un nouvel objet CMatrice,
+		   multiplication de la valeur passée en paramètre avec toutes les cases de la matrice.
+******************************************/
+
 template <class T>
 CMatrice<T> operator*(const T tValeur, const CMatrice<T> & MATmatrice);
+
+
+/*****************************************
+Opérateur <<.
+******************************************
+Entrée : un flux,
+		 une instance de CMatrice.
+Nécessite : rien.
+Sortie : un flux.
+Entraîne : l'affichage de la matrice.
+******************************************/
 
 template <class T>
 std::ostream & operator<<(std::ostream & OSTflux, const CMatrice<T> & MATmatrice);
