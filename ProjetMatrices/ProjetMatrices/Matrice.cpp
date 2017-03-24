b1c16c2a50177a8338b72796920098c16af1afe9
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2017-03-24 01:38:50 +0100

    fallait commit

--- ProjetMatrices/ProjetMatrices/Matrice.cpp
+++ ProjetMatrices/ProjetMatrices/Matrice.cpp
@@ -69,6 +69,7 @@ CMatrice<T> & CMatrice<T>::operator=(CMatrice<T> & MATmatrice)
 	unsigned int uiColonne;
 
 	// Initialisation
+
 	uiMATnbLignes = MATmatrice.uiMATnbLignes;
 	uiMATnbColonnes = MATmatrice.uiMATnbColonnes;
 	MATinitMatrice();
