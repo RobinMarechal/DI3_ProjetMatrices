ab2329d5e7b4852c3ddf157d2654259f841417cb
GouVsky <greg6-marco@hotmail.fr>
2017-03-25 18:19:40 +0100

    Création du programme principal.

--- ProjetMatrices/ProjetMatrices/main.cpp
+++ ProjetMatrices/ProjetMatrices/main.cpp
@@ -298,8 +298,81 @@ void robin()
 
 int main(int argc, char * argv[])
 {
-	robin();
+	//robin();
 	//gregoire();
+
+	if (argc > 1)
+	{
+		int iValeur;
+
+		CMatrice <double> * pcMATmatrices = (CMatrice <double> *) malloc(argc * sizeof(CMatrice <double>));
+
+		unsigned int uiBoucle;
+
+		for (uiBoucle = 1; uiBoucle < argc; uiBoucle++)
+		{
+			pcMATmatrices[uiBoucle - 1] = CParseur::PARparserFichier(argv[uiBoucle]);
+		}
+
+		cout << "Veuillez entrer une valeur : ";
+
+		cin >> iValeur;
+
+		// Affichage de la multiplication.
+
+		for (uiBoucle = 0; uiBoucle < argc; uiBoucle++)
+		{
+			(pcMATmatrices[uiBoucle] * iValeur).MATafficher();
+		}
+
+		// Affichage de la division.
+
+		for (uiBoucle = 0; uiBoucle < argc; uiBoucle++)
+		{
+			(pcMATmatrices[uiBoucle] / iValeur).MATafficher();
+		}
+
+		// Affichage de l'addition de toutes les matrices entre elles.
+
+		CMatrice <double> MATmatrice1;
+
+		for (uiBoucle = 0; uiBoucle < argc; uiBoucle++)
+		{
+			MATmatrice1 = MATmatrice1 + pcMATmatrices[uiBoucle];
+		}
+
+		MATmatrice1.MATafficher();
+
+		// Affichage de l'opération M1 - M2 + M3 - M4...
+
+		CMatrice <double> MATmatrice2;
+
+		for (uiBoucle = 0; uiBoucle < argc; uiBoucle++)
+		{
+			if (uiBoucle % 2 == 0)
+			{
+				MATmatrice2 = MATmatrice2 + pcMATmatrices[uiBoucle];
+			}
+
+			else
+			{
+				MATmatrice2 = MATmatrice2 - pcMATmatrices[uiBoucle];
+			}
+		}
+
+		(MATmatrice2).MATafficher();
+
+		// Affichage de la multiplication de toutes les matrices entre elles.
+
+		CMatrice <double> MATmatrice3;
+
+		for (uiBoucle = 0; uiBoucle < argc; uiBoucle++)
+		{
+			MATmatrice3 = MATmatrice3 * pcMATmatrices[uiBoucle];
+		}
+
+		(MATmatrice3).MATafficher();
+	}
 
 	return 0;
 }
